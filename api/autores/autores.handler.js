3d43715a6b1ddc7ec85e7705b507e6f13da863a8
Kenzohfs <matheus_hohmann@estudante.sc.senai.br>
2022-07-04 21:15:30 -0300

    crud autores

--- api/autores/autores.handler.js
+++ api/autores/autores.handler.js
@@ -1,26 +1,27 @@
+const { async } = require('@firebase/util');
 const {criar, get, getById, remove} = require('../../CRUD/index');
 
-const criarAutor = async (autor) => {
+async function criarAutor(autor){
     const novoAutor = await criar("autores", null, autor);
     return novoAutor;
 }
 
-const getAutores = async (req, res) => {
+async function getAutores(){
     const autores = await get("autores");
     return autores;
 };
 
-const getAutoresById = async (id) => {
+async function getAutoresById(id){
     const autor = await getById("autores", id);
     return autor;
 }
 
-const attAutor = async (id, autor) => {
+async function attAutor(id, autor){
     const attAutor = await criar("autores", id, autor);
     return attAutor;
 }
 
-const removerAutor = async (id) => {
+async function removerAutor(id){
     const autorRemovido = remove('autores', id);
     return autorRemovido;
 }
@@ -30,5 +31,6 @@ module.exports = {
     getAutores,
     getAutoresById,
     attAutor,
-    removerAutor
+    removerAutor,
+    async
 }
